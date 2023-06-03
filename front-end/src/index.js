66bd94d7c31eac8bbc211b47e513ec13a60419ee
RastkoAmun <raciosh@gmail.com>
2023-06-03 12:42:38 -0700

    Header and Footer added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css'
+import './assets/styles/bootstrap.custom.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
